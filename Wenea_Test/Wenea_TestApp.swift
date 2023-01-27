8bbe21562fd4df1ada315b8817c81954ed6ba0a3
Eloy Sanz <eloysn@gmail.com>
2023-01-27 08:39:39 +0100

    add files to proyect

---
+++ Wenea_Test/Wenea_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wenea_TestApp.swift
+//  Wenea_Test
+//
+//  Created by eloysn on 27/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Wenea_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
